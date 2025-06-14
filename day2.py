4df89ebdfeb57f09b14c639f42d3aba7d03ffb5d
Akshayanil1 <akshayy.anill@gmail.com>
2025-06-15 00:29:02 +0530

    second day of coding and second push

---
+++ day2.py
@@ -0,0 +1,11 @@
+book_title = "Ajaya roll of dice"
+pages = 350
+price = 300
+
+book_report = f""""
+Book Report:
+      Book Title: {book_title} {type(book_title)}
+      Pages: {pages} {type(pages)}
+      Price: {price} {type(price)}"""
+
+print(book_report)

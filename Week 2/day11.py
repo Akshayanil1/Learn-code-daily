5326e378e6bcf30636ac8dacaab75c7e56f923d7
Akshayanil1 <akshayy.anill@gmail.com>
2025-06-23 23:17:30 +0530

    starting with for loop

---
+++ Week 2/day11.py
@@ -0,0 +1,17 @@
+# favourite_movies = ["Iruvar", "Thalapathi", "Guru", "Soodhu Kavvum", "Saarpatta Parambarai"]
+# print("--- My favourite movies ---")
+# for movie in favourite_movies:
+#     print(f"One of my favorite movie is: {movie}")
+
+# numbers = [1, 2, 3, 4, 5, 10]
+
+# print("--- Squaring Numbers ---")
+
+# for number in numbers:
+#     square = number ** 2
+#     print(f"The square of {number} is: {square}")
+# print("\n--------------------------------------------")
+
+# for i in range(1, 11):
+#     square = i ** 2
+#     print(f"This square of {i} is: {square}")
